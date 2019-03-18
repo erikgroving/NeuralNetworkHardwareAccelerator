a54bfc444c979fd219e0bde79147a9d4912ec781
Erik Meade <egmeade@live.com>
2019-03-18 12:35:47 +0100

    update

--- SWModel/src/fullyconnected.h
+++ SWModel/src/fullyconnected.h
@@ -1,9 +1,13 @@
 #include <stdint.h>
+#include <vector>
 
 class FullyConnected {
 
     uint32_t input_size;
     uint32_t output_size;
+    std::vector<double> weights;
+    std::vector<double> 
+
 
 public:
     FullyConnected (uint32_t in, uint32_t out) : input_size(in), output_size(out) {}
