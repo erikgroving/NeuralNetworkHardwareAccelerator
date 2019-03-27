ff87d858aef8f9c51741beab3a57a0e5a5e0a850
erikgroving <egmeade@live.com>
2019-03-27 20:01:18 +0100

    100 random samples demo

--- SWModel/src/main.cpp
+++ SWModel/src/main.cpp
@@ -20,9 +20,9 @@ int main () {
 
     Net net(input_size, output_size, batch_size, lr, momentum);
 
-    Layer* fc = new FullyConnected(input_size, 32);
+    Layer* fc = new FullyConnected(input_size, 64);
     //Layer* fc2 = new FullyConnected(10, 4);
-    Layer* fc3 = new FullyConnected(32, output_size);
+    Layer* fc3 = new FullyConnected(64, output_size);
 
     std::vector< std::vector<double> > in; 
     std::vector<int> out; 
@@ -31,7 +31,7 @@ int main () {
     std::uniform_int_distribution<int> distribution_out(0, output_size - 1);  
     static unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     static std::default_random_engine generator(seed);
-    int test_size = 50;
+    int test_size = 100;
     for (int i = 0; i < test_size; i++) {
         std::vector<double> smpl;
         for (int j = 0; j < input_size; j++) {
