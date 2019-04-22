805de11f5a2a1258189488130189acb1f9c16141
erikgroving <egmeade@live.com>
2019-04-22 21:52:53 +0200

    Gradient check test for sw model needs to be done

---
+++ SWModel/test/gradient_check_test.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+
+#include <iostream>
+#include <random>
+#include <chrono>
+
+#include "../src/convolutional.h"
+#include "../src/fullyconnected.h"
+#include "../src/pooling.h"
+#include "../src/parse_data.h"
+#include "../src/layer.h" 
+#include "../src/net.h"
+
+TEST(GradientTest, GradientCheck) {
+    
+
+
+}
