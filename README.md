d5460943539605a05aac401d8f0ec08d415ccd76
Erik Meade <egmeade@live.com>
2019-05-13 17:59:45 +0200

    Update readme to reflect repo structure

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
 # Application Specific Hardware for Accelerating Neural Network Training Time
 
-### Overview
-Papers - This folder contains papers that I have read or think are useful.
+## Overview
+###FPGA    
+This folder contains the HDL for implementing my accelerator on an FPGA.
 
-### Notes from Papers
-I will write the main takeaways and insights from papers here for now (perhaps will migrate to a separate file later).
+###Models
+This folder contains PyTorch models of networks I have created. I used these while designing the architecture of my network that I am implementing for my FPGA. I will also use them for benchmarking professional library CPU and GPU training time.
+
+###Papers 
+This folder contains papers that I have read and think are useful.
+
+###Project Plan
+This folder contains the project plan for my master's thesis
+
+###SWModel
+This folder contains my own high-level software model for implementing any kind of neural network, with support for convolutional, pooling, and fullyconnected layers, using ReLU and softmax. The test files use googletest, the source of which can be found at the googletest repo. 
+
+###Weekly Presentations
+This folder contains my presentations that I have given throughout my project to my supervisor
+
+###misc
+This folder contains scripts that I have been using to debug the FPGA simulation output and generate coefficient files for initializing the weights in the BRAM
--- SWModel/test/gradient_check_test.cpp
+++ SWModel/test/gradient_check_test.cpp
@@ -11,7 +11,99 @@
 #include "../src/layer.h" 
 #include "../src/net.h"
 
-TEST(GradientTest, GradientCheck) {
+TEST(GradientTest, FCGradientCheck) {
+    
+    int input_size = 100;
+    int output_size = 2;
+    int batch_size = 1;
+    double momentum = 0.9;
+    double lr = 0.001; 
+    Net net(input_size, output_size, batch_size, lr, momentum);
+    
+
+    Layer* fc1 = new FullyConnected(input_size, 98);
+    Layer* fc2 = new FullyConnected(98, 64);
+    Layer* fc3 = new FullyConnected(64, output_size);
+
+    net.addLayer(fc1);
+    net.addLayer(fc2);
+    net.addLayer(fc3);
+
+    // Generate random input and output labels 
+    std::vector< std::vector<double> > in; 
+    std::vector<int> out; 
+
+    std::uniform_real_distribution<double> distribution(-1.0, 1.0);  
+    std::uniform_int_distribution<int> distribution_out(0, output_size - 1);  
+    static unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    static std::default_random_engine generator(seed);
+    int test_size = 10;
+    for (int i = 0; i < test_size; i++) {
+        std::vector<double> smpl;
+        for (int j = 0; j < input_size; j++) {
+            smpl.push_back(distribution(generator));
+        }
+        in.push_back(smpl);
+        out.push_back(distribution_out(generator));
+    }
+
+    int grad_tests = 100;
+    int num_layers = 3;
+    srand(time(0));
+    for (int i = 0; i < grad_tests; i++) {
+        double sigma = pow(10, -4);
+        auto out_ = net(in);
+        net.computeLossAndGradients(out);
+
+        /*for (size_t j = 0; j < out_[0].size(); j++) {
+            std::cout << out[0] << "\t\t" << out_[0][j] << "\t\t" << net.getOlGradient()[0][j] << "\n";
+        }*/
+
+        net.backpropLoss();
+
+        int l_idx = (rand() % num_layers);
+        FullyConnected* fc = (FullyConnected* )net.getLayers()[l_idx];
+        std::vector<Neuron>& neurons = fc->getNeurons();
+        int n_idx = (rand() % neurons.size());
+        auto neuron = neurons[n_idx];
+        auto weights = neuron.getWeights();
+        int w_idx = (rand() % neuron.getWeights().size());
+        double grad = neuron.getGradients()[w_idx];
+        net.clearSavedData();
+        
+
+        // + sigma loss
+        weights[w_idx] += sigma;
+        neurons[n_idx].setWeights(weights);
+        fc->setNeurons(neurons);
+        net(in);
+        double loss_plus = net.computeLossAndGradients(out);
+        net.clearSavedData();
+
+        // - sigma loss
+        weights[w_idx] -= (sigma + sigma);
+        neurons[n_idx].setWeights(weights);
+        fc->setNeurons(neurons);
+        net(in);
+        double loss_minus = net.computeLossAndGradients(out);
+        net.clearSavedData(); 
+        
+        weights[w_idx] += sigma;
+        neurons[n_idx].setWeights(weights);
+        fc->setNeurons(neurons);
+
+        double num_grad = (loss_plus - loss_minus) / (2 * sigma);
+        /*std::cout << "Original Loss: " << orig_loss;
+        std::cout << "\tCalculated Gradient: " << grad;
+        std::cout << "\tNumerical Gradient: " << num_grad << std::endl;*/
+
+        double diff = num_grad - grad;
+        diff = (diff > 0) ? diff : -diff;
+        ASSERT_LE(diff, 1e-6);
+    }
+}
+
+TEST(GradientTest, ConvGradientCheck) {
     
     int input_size = 100;
     int output_size = 2;
